0ccdad64d419b9e5dbf9be4d23bc587ede6bca0b
dziuniafanboi <mateusz.nowak02115@gmail.com>
2023-07-26 20:01:19 +0200

    strugling with segmentation fault when trying to correct moving functions

--- main.c
+++ main.c
@@ -7,98 +7,135 @@
 #include "libs/snake.h"
 #include "libs/map.h"
 
-//struct for holding original terminal settings
+// struct for holding original terminal settings
 struct termios orig_termios;
 
-//function to turn on "no buffor" mode
+// function to turn on "no buffor" mode
 void enableRawMode()
 {
     struct termios raw;
-    
+
     tcgetattr(STDIN_FILENO, &orig_termios);
     raw = orig_termios;
 
-    //turn of classical keyboard mode
+    // turn of classical keyboard mode
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-    //set minimal delay time and min char number
+    // set minimal delay time and min char number
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    //set my setting in terminal
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  
+    // set my setting in terminal
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-//function to turn off "no buffor" mode
+// function to turn off "no buffor" mode
 void disableRawMode()
 {
     tcsetattr(STDIN_FILENO, TCIFLUSH, &orig_termios);
 }
 
-void readKey(struct snake_node** head)
+void readKey(struct snake_node **head)
 {
     char c;
     char seq[3];
 
     read(STDIN_FILENO, &c, 1);
 
-    if (c == 'q')  // if 'q' is pressed
+    if (c == 'q') // if 'q' is pressed
     {
-        disableRawMode();  // turn off "no buffer" mode
-        exit(0);  // exit the program
+        disableRawMode(); // turn off "no buffer" mode
+        exit(0);          // exit the program
     }
 
-    if (c == '\x1b') 
+    if (c == '\x1b')
     {
         read(STDIN_FILENO, &seq[0], 1);
         read(STDIN_FILENO, &seq[1], 1);
 
         switch (seq[1])
         {
-             //up arrow
-    case 'A': 
-        vertical_movement(*head, 1);
-        break;
-    //down arrow
-    case 'B': 
-        vertical_movement(*head, -1);
-        break;
-    //right arrow
-    case 'C': 
-        horizontal_movement(*head, 1);
-        break;
-    //left arrow
-    case 'D': 
-        horizontal_movement(*head, -1);
-        break;
-    default: 
-        break;
+            // up arrow
+        case 'A':
+            vertical_movement(*head, 1);
+            break;
+        // down arrow
+        case 'B':
+            vertical_movement(*head, -1);
+            break;
+        // right arrow
+        case 'C':
+            horizontal_movement(*head, 1);
+            break;
+        // left arrow
+        case 'D':
+            horizontal_movement(*head, -1);
+            break;
+        default:
+            break;
         }
     }
 }
 
-
-void vertical_movement(struct snake_node* head, int increment)
+void vertical_movement(struct snake_node **head, int increment)
 {
-    while (head != NULL)
+    struct snake_node *current = *head;
+    int prevX = current->X, prevY = current->Y;
+    int tempX, tempY;
+
+    // move the head
+    current->X += increment;
+
+    // move the rest of the body
+    while (current != NULL)
     {
-        head->X += increment;
-        head = head->next;
+        // store current position
+        tempX = current->X;
+        tempY = current->Y;
+
+        // move to previous part's position
+        current->X = prevX;
+        current->Y = prevY;
+
+        // update previous position
+        prevX = tempX;
+        prevY = tempY;
+
+        current = current->next;
     }
 }
 
-void horizontal_movement(struct snake_node* head, int increment)
+void horizontal_movement(struct snake_node **head, int increment)
 {
-    while (head != NULL)
+    struct snake_node *current = *head;
+    int prevX = current->X, prevY = current->Y;
+    int tempX, tempY;
+
+    // move the head
+    current->Y += increment;
+
+    // move the rest of the body
+    while (current != NULL)
     {
-        head->Y += increment;
-        head = head->next;
+        // store current position
+        tempX = current->X;
+        tempY = current->Y;
+
+        // move to previous part's position
+        current->X = prevX;
+        current->Y = prevY;
+
+        // update previous position
+        prevX = tempX;
+        prevY = tempY;
+
+        current = current->next;
     }
 }
 
-void add(struct snake_node** head, int x, int y)
+void add(struct snake_node **head, int x, int y)
 {
-    struct snake_node* newNode = (struct snake_node*)malloc(sizeof(struct snake_node));
+    struct snake_node *newNode = (struct snake_node *)malloc(sizeof(struct snake_node));
     newNode->X = x;
     newNode->Y = y;
     newNode->next = NULL;
@@ -109,7 +146,7 @@ void add(struct snake_node** head, int x, int y)
     }
     else
     {
-        struct snake_node* current = *head;
+        struct snake_node *current = *head;
         while (current->next != NULL)
         {
             current = current->next;
@@ -120,30 +157,28 @@ void add(struct snake_node** head, int x, int y)
 
 int main(void)
 {
-    struct snake_node* head = NULL;
+    struct snake_node *head = NULL;
     char c;
 
     initialze_map();
 
     // Dodawanie 5 elementów do struktury
     add(&head, 10, 10);
-    add(&head, 20, 20);
-    add(&head, 30, 30);
-    add(&head, 40, 40);
-    add(&head, 50, 50);
+    add(&head, 10, 11);
+    add(&head, 10, 12);
+    add(&head, 10, 13);
+    add(&head, 10, 14);
 
     moving_test(head);
-    char direction;
 
     enableRawMode();
-    while(1)
-    {   
-    // move(&head);
-    readKey(&head);
-    moving_test(head);
+    while (1)
+    {
+        // move(&head);
+        readKey(&head);
+        moving_test(head);
     }
     disableRawMode();
 
     return 0;
 }
-
