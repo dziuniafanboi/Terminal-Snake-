d1b345d0cfeecf353cafa28ca61e5c24de15511e
dziuniafanboi <mateusz.nowak02115@gmail.com>
2023-07-26 18:25:31 +0200

    function to turn off no buffor mode

--- main.c
+++ main.c
@@ -7,30 +7,58 @@
 #include "libs/snake.h"
 #include "libs/map.h"
 
-void move(struct snake_node** head)
+//struct for holding original terminal settings
+struct termios orig_termios;
+
+//function to turn on "no buffor" mode
+void enableRawMode()
 {
-    while(_kbhit())
-    {
-        switch (getch())
-        {
-        case 'd':
-           horizontal_movement(*head, 1);
-            break;
-        case 'w':
-	        vertical_movement(*head, 1);
-            break;
-        case 's':
-    	    vertical_movement(*head, -1);
-            break;
-        case 'a':
-	        horizontal_movement(*head, -1);
-            break;
-        
-        default:
-            break;
-        }
-    }
+    struct termios raw;
+    
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    raw = orig_termios;
+
+    //turn of classical keyboard mode
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    //set minimal delay time and min char number
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    //set my setting in terminal
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  
 }
+
+//function to turn off "no buffor" mode
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCIFLUSH, &orig_termios);
+}
+
+// void move(struct snake_node** head)
+// {
+//     while(_kbhit())
+//     {
+//         switch (getch())
+//         {
+//         case 'd':
+//            horizontal_movement(*head, 1);
+//             break;
+//         case 'w':
+// 	        vertical_movement(*head, 1);
+//             break;
+//         case 's':
+//     	    vertical_movement(*head, -1);
+//             break;
+//         case 'a':
+// 	        horizontal_movement(*head, -1);
+//             break;
+        
+//         default:
+//             break;
+//         }
+//     }
+// }
 
 
 void vertical_movement(struct snake_node* head, int increment)
@@ -91,7 +119,7 @@ int main(void)
 
     while(1)
     {   
-    move(&head);
+    // move(&head);
 
     moving_test(head);
     }
